1174c3c3c034c0c27dddfecea5814af935cfab03
anton0777 <tsynsanton@gmail.com>
2025-07-10 14:28:42 +0200

    Added Post and Get endpoints. Connected the Prisma ORM

--- routes/endpoints.js
+++ routes/endpoints.js
@@ -1,8 +1,50 @@
 const express = require('express');
 const router = express.Router();
+const { PrismaClient } = require('../generated/prisma');
 
-router.get('/todo', (req,res)=>{
-    res.json({nodes: "hello world"});
+const prisma = new PrismaClient();
+
+router.get('/todo', async (req,res)=>{
+    // res.json({nodes: "hello world"});
+    try {
+        const tasks = await prisma.task.findMany()
+        res.json(tasks);
+    }
+    catch (err) {
+        res.status(500);
+        console.log(err);
+    }
+    res.end();
+})
+
+router.post('/todo', async (req, res) => {
+    try {
+        const {task} = req.body;
+        const newTask =
+            await prisma.task.create({data: {task}});
+        console.log('Created post:', newTask);
+    }
+    catch (err) {
+        res.status(500);
+        console.log(err);
+    }
+    res.end();
+})
+
+router.get('/todo/:id', async (req,res)=>{
+    const id = req.params.id;
+    try {
+        const task = await prisma.task.findFirst({
+            where: {
+                id: parseInt(id)
+            }
+        })
+        res.json(task);
+    }
+    catch (err) {
+        res.status(500);
+        console.log(err);
+    }
     res.end();
 })
 
