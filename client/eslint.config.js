df639b10921937f8346a4d15cc4191e35515b87d
Dmitriy Kovel <dkovel@scnsoft.com>
2025-09-14 17:54:50 +0200

    add new npm command, improve eslint config for front

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -3,9 +3,15 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 export default defineConfig([
   globalIgnores(['dist']),
+  // prettier,
+  eslintConfigPrettier,
+  eslintPluginPrettierRecommended,
+
   {
     files: ['**/*.{js,jsx}'],
     extends: [
