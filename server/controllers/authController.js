d8efa5b83ef680d14337f64a75fe8882979dde6b
anton0777 <tsynsanton@gmail.com>
2025-10-26 20:31:56 +0100

    #15: streamline user registration and login processes in authController and authService

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -6,8 +6,8 @@ export class AuthController {
   registerUser = async (req, res, next) => {
     try {
       const user = await this.authService.registerUser(req.body);
+
       res.status(201).json({
-        id: user.id,
         email: user.email,
         name: user.name,
         token: user.token,
@@ -20,8 +20,8 @@ export class AuthController {
   loginUser = async (req, res, next) => {
     try {
       const user = await this.authService.loginUser(req.body);
+
       res.status(200).json({
-        id: user.id,
         email: user.email,
         name: user.name,
         token: user.token,
--- server/services/authService.js
+++ server/services/authService.js
@@ -1,4 +1,3 @@
-import { CreateUser } from '../validators/userValidator.js';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
@@ -7,38 +6,42 @@ export class AuthService {
     this.authRepository = authRepository;
   }
 
-  registerUser = async (userData) => {
-    const parsedData = await CreateUser.parseAsync(userData);
-    parsedData.password = await bcrypt.hash(parsedData.password, 6);
-    const user = await this.authRepository.registerUser(parsedData);
-    user.token = await this.jwtSign(user);
+  registerUser = async (data) => {
+    data.password = await bcrypt.hash(data.password, 6);
+
+    const user = await this.authRepository.registerUser(data);
+
+    user.token = await this.generateToken(user);
+
     return user;
   };
 
-  loginUser = async (userData) => {
-    const { email, password } = userData;
-    const user = await this.authRepository.loginUser(email);
+  loginUser = async (data) => {
+    const user = await this.authRepository.loginUser(data.email);
+
+    user.token = await this.generateToken(user);
+
     if (!user) {
       throw new Error('Invalid email or password');
     }
+
     const { password: hashedPassword } =
-      await this.authRepository.hashedPassword(email);
-    const isPasswordValid = await bcrypt.compare(password, hashedPassword);
+      await this.authRepository.hashedPassword(data.email);
+
+    const isPasswordValid = await bcrypt.compare(data.password, hashedPassword);
+
     if (!isPasswordValid) {
       throw new Error('Invalid email or password');
     }
-    user.token = await this.jwtSign(user);
+
     return user;
   };
 
-  jwtSign = async (user) => {
+  async generateToken(user) {
     const { id: userId } = await this.authRepository.userId(user.email);
-    return jwt.sign(
-      {
-        id: userId,
-      },
-      process.env.JWT_SECRET,
-      { expiresIn: '1d' }
-    );
-  };
+
+    return jwt.sign({ id: userId }, process.env.JWT_SECRET, {
+      expiresIn: '1h',
+    });
+  }
 }
